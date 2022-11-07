7b60d79089c74bc05a712821f36cb1711c8d1a34
Dmitro <dimitriikislyackov@gmail.com>
2022-11-07 16:25:32 +0300

    3. Денежные переводы AccountStorage [#1104 #319560]

--- src/main/java/ru/job4j/storage/UserStorage.java
+++ src/main/java/ru/job4j/storage/UserStorage.java
@@ -3,47 +3,34 @@ package ru.job4j.storage;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    private Map<Integer, User> list = new ConcurrentHashMap<>();
+    private final Map<Integer, User> list = new ConcurrentHashMap<>();
 
-    synchronized void add(User user) {
-        list.putIfAbsent(user.getId(), user);
+    synchronized boolean add(User user) {
+        return list.putIfAbsent(user.getId(), user) == null;
     }
 
-    synchronized void update(User user) {
-        if (list.containsValue(user)) {
-            list.put(user.getId(), user);
-        }
+    synchronized boolean update(User user) {
+        return list.replace(user.getId(), user) == null;
     }
 
-    synchronized void delete(User user) {
-        if (list.containsValue(user)) {
-            list.remove(user.getId(), user);
-        }
-    }
-
-    synchronized User findById(int idUser) {
-        User user = null;
-        for (User user1 : list.values()) {
-            if (user1.getId() == idUser) {
-                user = user1;
-            }
-        }
-        return user;
+    synchronized boolean delete(User user) {
+        return list.remove(user.getId(), user);
     }
 
     synchronized boolean transfer(int fromID, int toId, int amount) {
-        User user1 = findById(fromID);
-        User user2 = findById(toId);
-        user1.setAmount(user1.getAmount() - amount);
-        user2.setAmount(user2.getAmount() + amount);
-        return true;
+        if (list.get(fromID) != null && list.get(toId) != null) {
+            if (list.get(fromID).getAmount() >= amount) {
+                list.get(fromID).setAmount(list.get(fromID).getAmount() - amount);
+                list.get(toId).setAmount(list.get(toId).getAmount() + amount);
+                return true;
+            }
+        }
+        return false;
     }
 }
