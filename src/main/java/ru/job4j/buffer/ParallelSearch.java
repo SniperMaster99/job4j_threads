0aa2daca6dc7e238916a720b8a9773e85012c84f
Dmitro <dimitriikislyackov@gmail.com>
2022-11-10 12:23:27 +0300

    2. Обеспечить остановку потребителя. [#66825 #321509]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -5,10 +5,16 @@ import ru.job4j.concurrent.SimpleBlockingQueue;
 public class ParallelSearch {
     public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        final int poisonPill = Integer.MAX_VALUE;
         final Thread consumer = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    System.out.println(queue.poll());
+                    int temp = queue.poll();
+                    if (temp == poisonPill) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    System.out.println(temp);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
@@ -20,6 +26,7 @@ public class ParallelSearch {
                 for (int index = 0; index < 5; index++) {
                     queue.offer(index);
                 }
+                queue.offer(poisonPill);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
